ac4bf198341c567989882b448387746b74f920bc
sarthakwhjr <73473647+sarthakwhjr@users.noreply.github.com>
2021-02-03 08:04:49 +0530

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -3,11 +3,11 @@ class Ball {
     var options = { 
       density: 1, 
       frictionAir: 0.005};
-
+    this.image=loadImage("images/s1.png")
     this.body = Bodies.rectangle(x, y, width, height, options);
     this.width = width;
     this.height = height;
-    this.image=loadImage("images/s1.png")
+
     World.add(world, this.body);
   }
 
